a92691ca6ec7e8760d7f7112e37963cef5f3e75f
Lasman Knia <lasmanknia@gmail.com>
2024-04-16 15:00:08 +0600

    Add files via upload

--- graph.js
+++ graph.js
@@ -22,19 +22,10 @@ function drawGraph(completionData) {
     ctx.moveTo(50, 20);
 
     let x = 50;
-    for (const [dateTime, completionPercentage] of lastSevenData) {
+    for (const [, completionPercentage] of lastSevenData) {
         const barHeight = (graphHeight - 50) * (completionPercentage / 100);
         ctx.fillStyle = 'hsl(154, 84%, 47%)';
         ctx.fillRect(x, graphHeight - barHeight, barWidth, barHeight);
-        ctx.fillStyle = 'white';
-
-        const dateObj = new Date(dateTime);
-        const dayName = dateObj.toLocaleDateString('en-US', { weekday: 'short' });
-        const time = dateObj.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
-
-        ctx.fillText(time, x + (barWidth - ctx.measureText(time).width) / 2, graphHeight - barHeight - 5);
-        ctx.fillText(dayName, x + (barWidth - ctx.measureText(dayName).width) / 2, graphHeight + 20);
-        ctx.fillText(completionPercentage + '%', x + (barWidth - ctx.measureText(completionPercentage + '%').width) / 2, graphHeight + 35);
 
         x += barSpacing;
     }
